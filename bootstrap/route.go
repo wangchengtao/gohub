f0e5ef20f0a35fe1abb3d5f6783b850666749ad1
wangchengtao <915129420@qq.com>
2022-03-02 18:40:21 +0800

    HTTP 访问日志

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"gohub/app/http/middlewares"
 	"gohub/routes"
 	"net/http"
 	"strings"
@@ -22,7 +23,7 @@ func SetupRoute(router *gin.Engine) {
 
 func registerGlobalMiddleWare(router *gin.Engine) {
 	router.Use(
-		gin.Logger(),
+		middlewares.Logger(),
 		gin.Recovery(),
 	)
 }
