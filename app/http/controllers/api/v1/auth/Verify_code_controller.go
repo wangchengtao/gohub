0f6685bf294861dbd14ee13a9fd2d7f92d0ad2fd
wangchengtao <915129420@qq.com>
2022-03-15 10:06:32 +0800

    发送短信验证码

--- app/http/controllers/api/v1/auth/Verify_code_controller.go
+++ app/http/controllers/api/v1/auth/Verify_code_controller.go
@@ -2,9 +2,11 @@ package auth
 
 import (
 	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/requests"
 	"gohub/pkg/captcha"
 	"gohub/pkg/logger"
 	"gohub/pkg/response"
+	"gohub/pkg/verifycode"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,4 +24,19 @@ func (vc *VerifyCodeController) ShowCaptcha(c *gin.Context) {
 		"captcha_id":    id,
 		"captcha_image": b64s,
 	})
+}
+
+func (vc *VerifyCodeController) SendUsingPhone(c *gin.Context) {
+	// 验证表单
+	request := requests.VerifyCodePhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.VerifyCodePhone); !ok {
+		return
+	}
+
+	// 发送 SMS
+	if ok := verifycode.NewVerifyCode().SendSMS(request.Phone); !ok {
+		response.Abort500(c, "发送短信失败")
+	} else {
+		response.Success(c)
+	}
 }
