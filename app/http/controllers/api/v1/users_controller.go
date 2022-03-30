e760f213537b6c6019fc7803b6b8b6a0dc4cc11d
wangchengtao <915129420@qq.com>
2022-03-30 20:49:42 +0800

    验证分页请求

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"gohub/app/models/user"
+	"gohub/app/requests"
 	"gohub/pkg/auth"
 	"gohub/pkg/response"
 
@@ -18,6 +19,11 @@ func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 }
 
 func (ctrl *UsersController) Index(c *gin.Context) {
+	request := requests.PaginationRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
+	
 	data, pager := user.Paginate(c, 10)
 
 	response.JSON(c, gin.H{
