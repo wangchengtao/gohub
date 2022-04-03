b874d520814238165a3c5d68be5c105b3f819fea
wangchengtao <915129420@qq.com>
2022-04-03 21:58:31 +0800

    API 域名和前缀

--- config/app.go
+++ config/app.go
@@ -12,6 +12,9 @@ func init() {
 			"key":      config.Env("APP_KEY", "33446a9dcf9ea060a0a6532b166da32f304af0de"),
 			"url":      config.Env("APP_URL", "http://localhost:3000"),
 			"timezone": config.Env("TIMEZONE", "Asia/Shanghai"),
+
+			// API 域名，未设置的话所有 API URL 加 api 前缀，如 http://domain.com/api/v1/users
+			"api_domain": config.Env("API_DOMAIN"),
 		}
 	})
 }
--- routes/api.go
+++ routes/api.go
@@ -4,12 +4,19 @@ import (
 	controllers "gohub/app/http/controllers/api/v1"
 	"gohub/app/http/controllers/api/v1/auth"
 	"gohub/app/http/middlewares"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterAPIRoutes(r *gin.Engine) {
-	v1 := r.Group("/v1")
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	var v1 *gin.RouterGroup
+	if len(config.Get("app.api_domain")) == 0 {
+		v1 = r.Group("/api/v1")
+	} else {
+		v1 = r.Group("/v1")
+	}
 
 	v1.Use(middlewares.LimitIP("200-H"))
 	{
