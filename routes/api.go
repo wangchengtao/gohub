a4914502be48eccb5bb96bc181318e01965a33b1
wangchengtao <915129420@qq.com>
2022-04-03 21:00:22 +0800

    修改邮箱

--- routes/api.go
+++ routes/api.go
@@ -47,6 +47,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		{
 			usersGroup.GET("", uc.Index)
 			usersGroup.PUT("", middlewares.AuthJWT(), uc.UpdateProfile)
+			usersGroup.PUT("/email", middlewares.AuthJWT(), uc.UpdateEmail)
+
 		}
 
 		cgc := new(controllers.CategoriesController)
